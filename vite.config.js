7838a614a39554bc2b7a07faca84bb6e70fdc78a
arijarvela <ari.jarvela@anfra.fi>
2025-08-08 14:29:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Vaihda 'kotinaytto' reposi nimeksi
-const basePath = '/infonaytto/'
-
 export default defineConfig({
   plugins: [react()],
-  base: basePath,
+  base: '/infonaytto/', // esim. '/infonaaytto/'
 })
